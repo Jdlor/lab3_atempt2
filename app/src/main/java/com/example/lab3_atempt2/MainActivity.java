4047453edf8a59736d25acda974e9284d059fa5c
Jdlor <Qwer06050506@gmail.com>
2024-03-29 19:12:27 +0600

    2

--- app/src/main/java/com/example/lab3_atempt2/MainActivity.java
+++ app/src/main/java/com/example/lab3_atempt2/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.lab3_atempt2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Binder;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.lab3_atempt2.databinding.ActivityMainBinding;
+
+import org.w3c.dom.Text;
+
+import java.time.format.TextStyle;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -21,7 +28,29 @@ public class MainActivity extends AppCompatActivity {
         binding.button.setOnClickListener(this::Send_Button);
     }
     private void Send_Button(View v){
-        Layout view = binding.textView.getLayout();
+        if(!binding.editTextText.getText().toString().matches("")) {
+            TextView newMessage = new TextView(binding.textView.getContext());
+            newMessage.setText(binding.editTextText.getText());
+            newMessage.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            newMessage.setTextSize(24);
+            newMessage.setTypeface(binding.textView.getTypeface());
+            binding.List.addView(newMessage);
+            binding.editTextText.setText("");
+            Answer(newMessage.getText().toString());
+        }
+    }
+    public void Answer(String message){
+        TextView newAnswer = new TextView(binding.textView.getContext());
+        if(message.matches("Привет")) {
+            newAnswer.setText("Пока");
+        }
+        else {
+            newAnswer.setText("Я не понимаю что значит *" + message + "*");
+        }
+        newAnswer.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+        newAnswer.setTextSize(24);
+        newAnswer.setTypeface(binding.textView.getTypeface());
+        binding.List.addView(newAnswer);
 
     }
 }
